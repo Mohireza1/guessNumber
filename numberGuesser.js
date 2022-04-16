68c1f4f4c83404b9073064b88210e66b0ef9a7b7
Mohireza <mohireza50@gmail.com>
2022-04-16 18:57:39 +0430

    JavaScript and HTML are ready

---
+++ numberGuesser.js
@@ -0,0 +1,32 @@
+"use strict";
+let guesser = () => {
+  const random = Math.floor(Math.random() * (100 - 1)) + 1;
+  alert(`I have a number in mind, what could it be?`);
+  let target = prompt(`What could the numebr be?`);
+  if (target === null) return;
+  let temp = Number(target);
+  while (isNaN(temp)) {
+    target = prompt("Please enter a number between 1 and 100");
+  }
+  let turn = 1;
+  while (temp !== random && target !== null) {
+    if (temp > 100 || temp < 1) {
+      target = prompt("Please enter a number between 1 and 100");
+      temp = Number(target);
+    } else if (random < temp) {
+      turn = turn + 1;
+      target = prompt("Too High!");
+      temp = Number(target);
+    } else if (temp < random) {
+      turn = turn + 1;
+      target = prompt("Too Low");
+      temp = Number(target);
+    }
+  }
+  if (temp === random) {
+    alert(
+      `You win! The number was ${random} and it took you ${turn} guesse\(s\) to guess it!`
+    );
+  }
+};
+guesser();
